7005c1e11dbe82585c712708e79820749150f833
= <=>
2023-09-01 17:56:20 +0200

    Initial commit

---
+++ src/components/MainBoard.js
@@ -0,0 +1,22 @@
+function MainBoard(){
+    // return <h1>Placeholder for chess board.</h1>
+
+    return (
+        <table>
+            <tr>
+                <th>Column1</th>
+                <th>Column2</th>
+                <th>Column3</th>
+            </tr>
+            <tr>
+                <th>a1</th>
+                <th>a1</th>
+                <th>a1</th>
+            </tr>
+        </table>
+    )
+}
+
+// const MainBoard = () => <h1>Placeholder for chess board.</h1>
+
+export default MainBoard
