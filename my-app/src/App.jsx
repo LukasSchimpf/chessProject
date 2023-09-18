f17c62587d3c18ec5ef337c22a786258c121ff11
LukasSchimpf <lukas.schimpf@gmail.com>
2023-09-18 14:00:27 +0200

    Created placeholder ChessBoard component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import ChessBoard from "./components/ChessBoard";
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
-  );
+      <div>
+        <header>
+          <h1>Chess.net (no relation)</h1>
+        </header>
+
+        <ChessBoard/>
+      </div>
+    );
 }
 
 export default App;
---
+++ my-app/src/components/ChessBoard.jsx
@@ -0,0 +1,8 @@
+export default function ChessBoard(){
+    return(
+        <div>
+            <h2>Chess Board</h2>
+            <p>Content.</p>
+        </div>
+    )
+}
