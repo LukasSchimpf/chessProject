3067b19c6487ccd891135ab70ae3718ca141e85a
LukasSchimpf <lukas.schimpf@gmail.com>
2023-09-27 16:59:17 +0200

    Moved board to its own component

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,61 +1,33 @@
 import { createSignal, from } from "solid-js";
 import { createEffect } from "solid-js";
 import {initBoard, createBoard, movePiece} from "./ChessLogic";
-
-function isCellWhite(index){
-  const isRowOdd = ((Math.floor(index/8)) %2 == 1);
-  const isCellOdd = (index % 2 == 1);
-
-  if(isRowOdd){
-    return isCellOdd;
-  }else{
-    return !isCellOdd;
-  }
-}
+import ChessBoard from './components/ChessBoard'
 
 function App() {
   const [board, setBoard] = createSignal(initBoard(createBoard()));
   const [whitePerspective, setWhitePerspective] = createSignal(true);
 
   console.log(board());
-  return (
+
+  const indexes = Array.from(Array(64).keys());
+  console.log(indexes)
+
+ return(
+  <div>
+    <ChessBoard whitePerspective={whitePerspective()} board={board()} />
+
     <div>
-      <div class=" grid grid-cols-8">
-      <For each={whitePerspective()? board() : board().toReversed()}>{
-        (square, index)=>{
-
-          const cellColor = isCellWhite(index())? " bg-light-square": " bg-dark-square";
-          const cellCoordinateColor = !isCellWhite(index())? " text-light-square":" text-dark-square"
-
-          const pieceColor = square && square.isWhite? " text-black-piece": " text-white-piece";
-          const cellContent = square? square.type : "";
-
-          return <div class={" aspect-square" + cellColor}>
-            <div class={"text-xs" + cellCoordinateColor}>
-              a1
-            </div>
-            <div class={" text-4xl text-center"+ pieceColor}>
-              {cellContent}
-            </div>
-          </div>;
-
-        }
-        }
-      </For>
-
-      </div>
-      <div>
-        <button onClick={() =>{setWhitePerspective(!whitePerspective());
-        console.log("Flipped Perspective")}}>Flip Board</button>
-      </div>
-      <div>
-        <button onclick={() =>{setBoard(movePiece(board(), "a", 2, "a", 3));
-        console.log("Moved Piece");
-        console.log(board())
-        }}>Advance the white A Pawn</button>
-      </div>
+      <button onClick={() =>{setWhitePerspective(!whitePerspective());
+      console.log("Flipped Perspective")}}>Flip Board</button>
     </div>
-  );
+    <div>
+      <button onclick={() =>{setBoard(movePiece(board(), "a", 2, "a", 3));
+      console.log("Moved Piece");
+      console.log(board())
+      }}>Advance the white A Pawn</button>
+    </div>
+  </div>
+ )
 }
 
 export default App;
--- my-app/src/components/ChessBoard.jsx
+++ my-app/src/components/ChessBoard.jsx
@@ -1,77 +1,39 @@
-export default function ChessBoard(){
+
+function isCellWhite(index){
+  const isRowOdd = ((Math.floor(index/8)) %2 == 1);
+  const isCellOdd = (index % 2 == 1);
+
+  if(isRowOdd){
+    return isCellOdd;
+  }else{
+    return !isCellOdd;
+  }
+}
+
+export default function ChessBoard(props){
     return(
-        <div class="container columns-8 gap-0 ...">
-            <div class="w-full aspect-square bg-black text-white ...">A1</div>
-            <div class="w-full aspect-square bg-white text-black ...">A2</div>
-            <div class="w-full aspect-square bg-black text-white ...">A3</div>
-            <div class="w-full aspect-square bg-white text-black ...">A4</div>
-            <div class="w-full aspect-square bg-black text-white ...">A5</div>
-            <div class="w-full aspect-square bg-white text-black ...">A6</div>
-            <div class="w-full aspect-square bg-black text-white ...">A7</div>
-            <div class="w-full aspect-square bg-white text-black ...">A8</div>
+      <div class=" grid grid-cols-8">
+      <For each={props.whitePerspective? props.board : props.board.toReversed()}>{
+        (square, index)=>{
 
-            <div class="w-full aspect-square bg-white text-black ...">B1</div>
-            <div class="w-full aspect-square bg-black text-white ...">B2</div>
-            <div class="w-full aspect-square bg-white text-black ...">B3</div>
-            <div class="w-full aspect-square bg-black text-white ...">B4</div>
-            <div class="w-full aspect-square bg-white text-black ...">B5</div>
-            <div class="w-full aspect-square bg-black text-white ...">B6</div>
-            <div class="w-full aspect-square bg-white text-black ...">B7</div>
-            <div class="w-full aspect-square bg-black text-white ...">B8</div>
+          const cellColor = isCellWhite(index())? " bg-light-square": " bg-dark-square";
+          const cellCoordinateColor = !isCellWhite(index())? " text-light-square":" text-dark-square"
 
-            <div class="w-full aspect-square bg-black text-white ...">C1</div>
-            <div class="w-full aspect-square bg-white text-black ...">C2</div>
-            <div class="w-full aspect-square bg-black text-white ...">C3</div>
-            <div class="w-full aspect-square bg-white text-black ...">C4</div>
-            <div class="w-full aspect-square bg-black text-white ...">C5</div>
-            <div class="w-full aspect-square bg-white text-black ...">C6</div>
-            <div class="w-full aspect-square bg-black text-white ...">C7</div>
-            <div class="w-full aspect-square bg-white text-black ...">C8</div>
+          const pieceColor = square && square.isWhite? " text-black-piece": " text-white-piece";
+          const cellContent = square? square.type : "";
 
-            <div class="w-full aspect-square bg-white text-black ...">D1</div>
-            <div class="w-full aspect-square bg-black text-white ...">D2</div>
-            <div class="w-full aspect-square bg-white text-black ...">D3</div>
-            <div class="w-full aspect-square bg-black text-white ...">D4</div>
-            <div class="w-full aspect-square bg-white text-black ...">D5</div>
-            <div class="w-full aspect-square bg-black text-white ...">D6</div>
-            <div class="w-full aspect-square bg-white text-black ...">D7</div>
-            <div class="w-full aspect-square bg-black text-white ...">D8</div>
+          return <div class={" aspect-square" + cellColor}>
+            <div class={"text-xs" + cellCoordinateColor}>
+              a1
+            </div>
+            <div class={" text-4xl text-center"+ pieceColor}>
+              {cellContent}
+            </div>
+          </div>;
 
-            <div class="w-full aspect-square bg-black text-white ...">E1</div>
-            <div class="w-full aspect-square bg-white text-black ...">E2</div>
-            <div class="w-full aspect-square bg-black text-white ...">E3</div>
-            <div class="w-full aspect-square bg-white text-black ...">E4</div>
-            <div class="w-full aspect-square bg-black text-white ...">E5</div>
-            <div class="w-full aspect-square bg-white text-black ...">E6</div>
-            <div class="w-full aspect-square bg-black text-white ...">E7</div>
-            <div class="w-full aspect-square bg-white text-black ...">E8</div>
-
-            <div class="w-full aspect-square bg-white text-black ...">F1</div>
-            <div class="w-full aspect-square bg-black text-white ...">F2</div>
-            <div class="w-full aspect-square bg-white text-black ...">F3</div>
-            <div class="w-full aspect-square bg-black text-white ...">F4</div>
-            <div class="w-full aspect-square bg-white text-black ...">F5</div>
-            <div class="w-full aspect-square bg-black text-white ...">F6</div>
-            <div class="w-full aspect-square bg-white text-black ...">F7</div>
-            <div class="w-full aspect-square bg-black text-white ...">F8</div>
-
-            <div class="w-full aspect-square bg-black text-white ...">G1</div>
-            <div class="w-full aspect-square bg-white text-black ...">G2</div>
-            <div class="w-full aspect-square bg-black text-white ...">G3</div>
-            <div class="w-full aspect-square bg-white text-black ...">G4</div>
-            <div class="w-full aspect-square bg-black text-white ...">G5</div>
-            <div class="w-full aspect-square bg-white text-black ...">G6</div>
-            <div class="w-full aspect-square bg-black text-white ...">G7</div>
-            <div class="w-full aspect-square bg-white text-black ...">G8</div>
-
-            <div class="w-full aspect-square bg-white text-black ...">H1</div>
-            <div class="w-full aspect-square bg-black text-white ...">H2</div>
-            <div class="w-full aspect-square bg-white text-black ...">H3</div>
-            <div class="w-full aspect-square bg-black text-white ...">H4</div>
-            <div class="w-full aspect-square bg-white text-black ...">H5</div>
-            <div class="w-full aspect-square bg-black text-white ...">H6</div>
-            <div class="w-full aspect-square bg-white text-black ...">H7</div>
-            <div class="w-full aspect-square bg-black text-white ...">H8</div>
-        </div>
+        }
+        }
+      </For>
+      </div>
     )
 }
